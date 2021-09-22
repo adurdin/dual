f0b3797f16c4e0b35d8dda69c7ff400c1b90b103
Andy Durdin <me@andy.durdin.net>
2021-09-22 13:21:46 +0100

    sealed and broken jar variants.

--- objsrc/make_bigjar.bat
+++ objsrc/make_bigjar.bat
@@ -2,5 +2,8 @@
 REM bsp bigjar.e bigjar.bin -l3 -V
 REM bsp bigjarlid.e bigjarlid.bin -l3 -V
 copy /y bigjar.bin ..\obj\
+copy /y bigjar1.bin ..\obj\
+copy /y bigjar2.bin ..\obj\
 copy /y bigjarlid.bin ..\obj\
+copy /y bigjarlid1.bin ..\obj\
 copy /y bigjar1.png ..\obj\txt\
