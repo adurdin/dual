d85ad6ebc26e0077676ec2d3596474d25a7c51ca
Andy Durdin <me@andy.durdin.net>
2021-09-23 05:05:54 +0100

    make smaller doorstone2. add doors to all the tombs.

--- objsrc/make_doorstone.bat
+++ objsrc/make_doorstone.bat
@@ -1,4 +1,6 @@
 @echo off
 bsp doorstone.e doorstone.bin -l3 -V
+bsp doorstone2.e doorstone2.bin -l3 -V
 copy /y doorstone.bin ..\obj\
+copy /y doorstone2.bin ..\obj\
 copy /y doorstone.png ..\obj\txt\
