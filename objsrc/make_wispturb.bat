8f84fe9cd9f1e632eac72a7fcb76f45269477fe4
Andy Durdin <me@andy.durdin.net>
2021-09-24 05:40:07 +0100

    realign wispturb model centers so they can be positioned, exactly, on-grid.

--- objsrc/make_wispturb.bat
+++ objsrc/make_wispturb.bat
@@ -1,9 +1,9 @@
 @echo off
 REM bsp wispturb.e wispturb.bin -l3 -V
-bsp wispcavity.e wispcavity.bin -l3 -V -o
+bsp wispcavity.e wispcavity.bin -l3 -V
 rem increased subobject shrink so bsp doesnt complain about "merging one atomic, one not"
-bsp wisphatch.e wisphatch.bin -l3 -V -o -es2
-bsp wispbox.e wispbox.bin -l3 -V -o
+bsp wisphatch.e wisphatch.bin -l3 -V -es2
+bsp wispbox.e wispbox.bin -l3 -V
 copy /y wispturb.bin ..\obj\
 copy /y wisphatch.bin ..\obj\
 copy /y wispcavity.bin ..\obj\
