e0377846c6ff25b900fe5fc5d02e38df5dd4ea7a
Andy Durdin <me@andy.durdin.net>
2021-12-26 10:49:32 +0000

    28x4 columns (for laboratory tower)

--- objsrc/make_cols.bat
+++ objsrc/make_cols.bat
@@ -1,7 +1,11 @@
 @echo off
 bsp col14x4.e col14x4.bin -l3 -V
 bsp cols14x4.e cols14x4.bin -l3 -V
+bsp col28x4.e col28x4.bin -l3 -V
+bsp cols28x4.e cols28x4.bin -l3 -V
 copy /y col14x4.bin ..\obj\
 copy /y cols14x4.bin ..\obj\
+copy /y col28x4.bin ..\obj\
+copy /y cols28x4.bin ..\obj\
 copy /y col01.png ..\obj\txt\
 copy /y col02.png ..\obj\txt\
