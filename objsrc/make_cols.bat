72ec5c6f60a3c4e9bacd1eaa1c30b10a9087611d
Andy Durdin <me@andy.durdin.net>
2021-12-26 11:01:11 +0000

    use 85 degree smoothing angle for square columns

--- objsrc/make_cols.bat
+++ objsrc/make_cols.bat
@@ -1,8 +1,8 @@
 @echo off
 bsp col14x4.e col14x4.bin -l3 -V
-bsp cols14x4.e cols14x4.bin -l3 -V
+bsp cols14x4.e cols14x4.bin -l3 -V -M0.087156
 bsp col28x4.e col28x4.bin -l3 -V
-bsp cols28x4.e cols28x4.bin -l3 -V
+bsp cols28x4.e cols28x4.bin -l3 -V -M0.087156
 copy /y col14x4.bin ..\obj\
 copy /y cols14x4.bin ..\obj\
 copy /y col28x4.bin ..\obj\
