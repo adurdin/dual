83852b7c6cffa283ba917df98f77b07a3b92ab34
Andy Durdin <me@andy.durdin.net>
2022-01-14 09:39:16 +0000

    adding detail to the periapt model

--- meshsrc/make_periaptv.bat
+++ meshsrc/make_periaptv.bat
@@ -2,5 +2,6 @@
 meshbld.exe periaptv.e periaptv.bin simple.map -V
 copy /y periaptv.bin ..\mesh\
 copy /y peri1.png ..\mesh\txt16\
+copy /y peri2.png ..\mesh\txt16\
 copy /y pericry1.png ..\mesh\txt16\
 copy /y perihand.png ..\mesh\txt16\
