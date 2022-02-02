2bb2afdc5feb282d0ad706588ab605df8aa6b82c
Andy Durdin <me@andy.durdin.net>
2022-02-02 06:07:00 +0000

    detail ruined guard training room and common room.

---
+++ objsrc/make_firep03x.bat
@@ -0,0 +1,7 @@
+@echo off
+REM bsp firep03a.e firep03a.bin -l3 -V
+REM bsp firep03x.e firep03x.bin -l3 -V
+copy /y firep03a.bin ..\obj\
+copy /y firep03x.bin ..\obj\
+copy /y firep03a.png ..\obj\txt16\
+copy /y firep03x.png ..\obj\txt16\
