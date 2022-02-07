158916ce1db8c45ee895c94d787996fd1cc9958c
Andy Durdin <me@andy.durdin.net>
2022-02-07 02:08:04 +0000

    adchandel: art deco chandelier. tweaked lighting in library. tweaked some portraits to have a more even brightness

---
+++ objsrc/make_adchandel.bat
@@ -0,0 +1,6 @@
+@echo off
+REM bsp adchandel.e adchandel.bin -l3 -V
+copy /y adchandel.bin ..\obj\
+REM copy /y adliteb.png ..\obj\txt16\
+REM copy /y adlitegb.png ..\obj\txt16\
+REM copy /y towarm2.png ..\obj\txt16\
