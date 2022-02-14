6f9df238b3bce48ddbdc69d77746903b3d384170
Andy Durdin <me@andy.durdin.net>
2022-02-14 09:43:18 +0000

    finished the dining room. new wooden column and dining table models.

---
+++ objsrc/make_colwood.bat
@@ -0,0 +1,4 @@
+@echo off
+REM bsp colwood.e colwood.bin -l3 -V
+copy /y colwood.bin ..\obj\
+copy /y colwood.png ..\obj\txt16\
---
+++ objsrc/make_tabledin.bat
@@ -0,0 +1,6 @@
+@echo off
+REM bsp tabledin.e tabledin.bin -l3 -V
+REM bsp tabledin2.e tabledin2.bin -l3 -V
+copy /y tabledin.bin ..\obj\
+copy /y tabledin2.bin ..\obj\
+copy /y tabledin.png ..\obj\txt16\
