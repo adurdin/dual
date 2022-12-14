fce21548c82e9d4fa854aa634f7da2fd9d9d3304
Andy Durdin <me@andy.durdin.net>
2022-12-14 04:40:29 +0000

    viccamosw1a, viccamosw1b - victorian camouflaged button, with frame.

---
+++ objsrc/make_viccamosw1.bat
@@ -0,0 +1,6 @@
+@echo off
+REM bsp viccamosw1a.e viccamosw1a.bin -l3 -V
+REM bsp viccamosw1b.e viccamosw1b.bin -l3 -V
+copy /y viccamosw1a.bin ..\obj\
+copy /y viccamosw1b.bin ..\obj\
+copy /y viccamosw1.png ..\obj\txt16\
