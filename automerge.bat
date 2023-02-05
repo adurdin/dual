87a34a71c09076169c89db67f8adfc48f641578c
Andy Durdin <me@andy.durdin.net>
2023-02-05 23:43:13 +0000

    automerge: misbehaving! don't overwrite miss20 for now.

--- automerge.bat
+++ automerge.bat
@@ -28,14 +28,14 @@ for /f "delims=" %%i in ('git status --short --untracked=no --porcelain -- %MISN
    set "changed=!changed!,!tmp!"
 )
 if "%changed%"=="" (
-    goto startmerge
+    goto startexport
 ) else (
     echo Aborting, uncommitted changes in:
     echo %changed:~2%
 )
 goto end
 
-:startmerge
+:startexport
 set script=%FMDIR%\automerge.dromed
 if not exist %script% goto noscript
 
@@ -49,6 +49,7 @@ pushd %T2DIR%
 dromed.exe -fm=%FM% edit_script_StartUp=%script% mergename=%MISNAME% mergeoptim=1 mergelighting=0
 popd
 
+:startmerge
 if not exist %FMDIR%\_slice1.mis goto badmerge
 if not exist %FMDIR%\_slice2.mis goto badmerge
 if not exist %FMDIR%\_slice3.mis goto badmerge
@@ -61,7 +62,7 @@ misdeed merge _combined1.mis _slice3.mis 0 0 1 96 -o _combined2.mis
 if not exist %FMDIR%\_combined2.mis goto badmerge
 
 REM looks good! lets write over the original mission now, and clean up
-copy /y %FMDIR%\_combined2.mis %FMDIR%\%MISNAME%.mis
+rem copy /y %FMDIR%\_combined2.mis %FMDIR%\%MISNAME%.mis
 
 REM -- we could clean up , but lets leave the output files here for debugging.
 rem if exist %FMDIR%\_slice1.mis del %FMDIR%\_slice1.mis
