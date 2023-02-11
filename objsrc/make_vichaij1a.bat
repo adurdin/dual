65860bb28d6c70020f96ea81d3de77955f9ecc0d
Andy Durdin <me@andy.durdin.net>
2023-02-11 06:55:40 +0000

    start ruining west wing main hall. realise the unruined version needs more love first!

---
+++ objsrc/make_vichaij1a.bat
@@ -0,0 +1,10 @@
+@echo off
+REM bsp vichaij1a.e vichaij1a.bin -l3 -V
+REM bsp vichaij1ax.e vichaij1ax.bin -l3 -V
+REM bsp vichaij1ay.e vichaij1ay.bin -l3 -V
+copy /y vichaij1a.bin ..\obj\
+copy /y vichaij1ax.bin ..\obj\
+copy /y vichaij1ay.bin ..\obj\
+copy /y vicobl1a.png ..\obj\txt16\
+copy /y vicobl1ax.png ..\obj\txt16\
+copy /y vicobl1ay.png ..\obj\txt16\
---
+++ objsrc/make_vicobl1a.bat
@@ -0,0 +1,10 @@
+@echo off
+REM bsp vicobl1a.e vicobl1a.bin -l3 -V
+REM bsp vicobl1ax.e vicobl1ax.bin -l3 -V
+REM bsp vicobl1ay.e vicobl1ay.bin -l3 -V
+copy /y vicobl1a.bin ..\obj\
+copy /y vicobl1ax.bin ..\obj\
+copy /y vicobl1ay.bin ..\obj\
+copy /y vicobl1a.png ..\obj\txt16\
+copy /y vicobl1ax.png ..\obj\txt16\
+copy /y vicobl1ay.png ..\obj\txt16\
