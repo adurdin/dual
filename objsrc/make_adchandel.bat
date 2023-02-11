e57d02b3e5dfa7c6919fdeb150e3bbdefe520cfc
Andy Durdin <me@andy.durdin.net>
2023-02-11 01:51:57 +0000

    ruined coftab2*, adchandel*

--- objsrc/make_adchandel.bat
+++ objsrc/make_adchandel.bat
@@ -1,6 +1,9 @@
 @echo off
-REM bsp adchandel.e adchandel.bin -l3 -V
-copy /y adchandel.bin ..\obj\
-REM copy /y adliteb.png ..\obj\txt16\
-REM copy /y adlitegb.png ..\obj\txt16\
-REM copy /y towarm2.png ..\obj\txt16\
+REM bsp adchandela.e adchandela.bin -l3 -V
+REM bsp adchandelax.e adchandelax.bin -l3 -V
+REM bsp adchandelay.e adchandelay.bin -l3 -V
+copy /y adchandela.bin ..\obj\
+copy /y adchandelax.bin ..\obj\
+copy /y adchandelay.bin ..\obj\
+copy /y adchandela.png ..\obj\txt16\
+copy /y adchandelax.png ..\obj\txt16\
---
+++ objsrc/make_coftab2a.bat
@@ -0,0 +1,10 @@
+@echo off
+REM bsp coftab2a.e coftab2a.bin -l3 -V
+REM bsp coftab2ax.e coftab2ax.bin -l3 -V
+REM bsp coftab2ay.e coftab2ay.bin -l3 -V
+copy /y coftab2a.bin ..\obj\
+copy /y coftab2ax.bin ..\obj\
+copy /y coftab2ay.bin ..\obj\
+copy /y coftab2a.png ..\obj\txt16\
+copy /y coftab2ax.png ..\obj\txt16\
+copy /y coftab2ay.png ..\obj\txt16\
