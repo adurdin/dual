ece0739d1de1c36ecd921f9e32e7cda503be9052
Andy Durdin <me@andy.durdin.net>
2023-02-13 07:40:52 +0000

    painting7x6 model

--- objsrc/make_paintinga.bat
+++ objsrc/make_paintinga.bat
@@ -1,12 +1,14 @@
 @echo off
 REM bsp paintinga6x6.e paintinga6x6.bin -l3 -V
-REM bsp paintinga4x8.e paintinga.bi4x8n -l3 -V
-REM bsp paintinga6x8.e paintinga.bin -6x8l3 -V
-REM bsp paintinga8x4.e paintinga.bin -l3 8x4-V
-REM bsp paintinga8x6.e paintinga.bin -l3 -V8x6
+REM bsp paintinga4x8.e paintinga4x8.bin -l3 -V
+REM bsp paintinga6x8.e paintinga6x8.bin -l3 -V
+REM bsp paintinga8x4.e paintinga8x4.bin -l3 -V
+REM bsp paintinga8x6.e paintinga8x6.bin -l3 -V
+REM bsp paintinga7x6.e paintinga7x6.bin -l3 -V
 copy /y paintinga6x6.bin ..\obj
 copy /y paintinga4x8.bin ..\obj
 copy /y paintinga6x8.bin ..\obj
 copy /y paintinga8x4.bin ..\obj
 copy /y paintinga8x6.bin ..\obj
+copy /y paintinga7x6.bin ..\obj
 copy /y paintinga.png ..\obj\txt16\
