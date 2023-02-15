b51f6f55e49a68ef5329dc5662de3a9547812c4b
Andy Durdin <me@andy.durdin.net>
2023-02-15 08:07:22 +0000

    plantst1a model, with ruined versions.

---
+++ objsrc/make_plantst1a.bat
@@ -0,0 +1,10 @@
+@echo off
+REM bsp plantst1a.e plantst1a.bin -l3 -V
+REM bsp plantst1ax.e plantst1ax.bin -l3 -V
+REM bsp plantst1ay.e plantst1ay.bin -l3 -V
+copy /y plantst1a.bin ..\obj\
+copy /y plantst1ax.bin ..\obj\
+copy /y plantst1ay.bin ..\obj\
+copy /y plantst1a.png ..\obj\txt16\
+copy /y plantst1ax.png ..\obj\txt16\
+copy /y plantst1ay.png ..\obj\txt16\
