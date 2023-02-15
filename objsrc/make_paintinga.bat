32c11b549f5256efeb969f8a95957934db8d5e2a
Andy Durdin <me@andy.durdin.net>
2023-02-15 08:05:15 +0000

    ruined painting models.

--- objsrc/make_paintinga.bat
+++ objsrc/make_paintinga.bat
@@ -5,10 +5,23 @@ REM bsp paintinga6x8.e paintinga6x8.bin -l3 -V
 REM bsp paintinga8x4.e paintinga8x4.bin -l3 -V
 REM bsp paintinga8x6.e paintinga8x6.bin -l3 -V
 REM bsp paintinga7x6.e paintinga7x6.bin -l3 -V
+REM bsp paintinga6x6x.e paintinga6x6x.bin -l3 -V
+REM bsp paintinga4x8x.e paintinga4x8x.bin -l3 -V
+REM bsp paintinga6x8x.e paintinga6x8x.bin -l3 -V
+REM bsp paintinga8x4x.e paintinga8x4x.bin -l3 -V
+REM bsp paintinga8x6x.e paintinga8x6x.bin -l3 -V
+REM bsp paintinga7x6x.e paintinga7x6x.bin -l3 -V
 copy /y paintinga6x6.bin ..\obj
 copy /y paintinga4x8.bin ..\obj
 copy /y paintinga6x8.bin ..\obj
 copy /y paintinga8x4.bin ..\obj
 copy /y paintinga8x6.bin ..\obj
 copy /y paintinga7x6.bin ..\obj
+copy /y paintinga6x6x.bin ..\obj
+copy /y paintinga4x8x.bin ..\obj
+copy /y paintinga6x8x.bin ..\obj
+copy /y paintinga8x4x.bin ..\obj
+copy /y paintinga8x6x.bin ..\obj
+copy /y paintinga7x6x.bin ..\obj
 copy /y paintinga.png ..\obj\txt16\
+copy /y paintingax.png ..\obj\txt16\
