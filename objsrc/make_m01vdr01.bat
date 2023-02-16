c934ffee84b988c89514b00c34a992dcff4c0d33
Andy Durdin <me@andy.durdin.net>
2023-02-16 16:00:03 +0000

    grimy and mossy versions of all vicm doors.

--- objsrc/make_m01vdr01.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM bsp m01vdr01x.e m01vdr01x.bin -l3 -V
-REM bsp m01vdr01y.e m01vdr01y.bin -l3 -V
-copy /y m01vdr01x.bin ..\obj\
-copy /y m01vdr01y.bin ..\obj\
-copy /y vdrm0101x.png ..\obj\txt16\
-copy /y vdrm0101y.png ..\obj\txt16\
