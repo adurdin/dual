b89818e924c9dbdbe35ae63c8a7539ae2b03a902
Andy Durdin <me@andy.durdin.net>
2023-02-17 07:05:06 +0000

    ruined fancoucha

---
+++ objsrc/make_fancoucha.bat
@@ -0,0 +1,12 @@
+@echo off
+REM bsp fancoucha.e fancoucha.bin -l3 -V
+REM bsp fancouchax.e fancouchax.bin -l3 -V
+REM bsp fancouchay.e fancouchay.bin -l3 -V
+REM bsp fancouchaz.e fancouchaz.bin -l3 -V
+copy /y fancoucha.bin ..\obj\
+copy /y fancouchax.bin ..\obj\
+copy /y fancouchay.bin ..\obj\
+copy /y fancouchaz.bin ..\obj\
+copy /y fancoucha.png ..\obj\txt16\
+copy /y fancouchax.png ..\obj\txt16\
+copy /y fancouchay.png ..\obj\txt16\
