2c79e6faa10552325310443d0e4cf254179db0f2
Andy Durdin <me@andy.durdin.net>
2023-02-17 05:43:04 +0000

    ruined coftab1a

---
+++ objsrc/make_coftab1a.bat
@@ -0,0 +1,9 @@
+@echo off
+REM bsp coftab1a.e coftab1a.bin -l3 -V
+REM bsp coftab1ax.e coftab1ax.bin -l3 -V
+REM bsp coftab1ay.e coftab1ay.bin -l3 -V
+copy /y coftab1a.bin ..\obj\
+copy /y coftab1ax.bin ..\obj\
+copy /y coftab1ay.bin ..\obj\
+copy /y sidtab1.png ..\obj\txt16\
+copy /y sidtab1x.png ..\obj\txt16\
