df062b9f3301571c721f127bfe2494c31ac69812
Andy Durdin <me@andy.durdin.net>
2023-02-24 15:18:29 +0000

    ruined VicArmchair variants

---
+++ objsrc/make_fanchaira.bat
@@ -0,0 +1,10 @@
+@echo off
+REM bsp fanchaira.e fanchaira.bin -l3 -V
+REM bsp fanchairax.e fanchairax.bin -l3 -V
+REM bsp fanchairay.e fanchairay.bin -l3 -V
+copy /y fanchaira.bin ..\obj\
+copy /y fanchairax.bin ..\obj\
+copy /y fanchairay.bin ..\obj\
+copy /y fancoucha.png ..\obj\txt16\
+copy /y fancouchax.png ..\obj\txt16\
+copy /y fancouchay.png ..\obj\txt16\
