9a97041853861cc9d523040face576be075ba912
Andy Durdin <me@andy.durdin.net>
2023-02-24 17:03:25 +0000

    ruined west wing stepped hallway.

--- objsrc/make_colwood.bat
+++ objsrc/make_colwood.bat
@@ -1,4 +1,10 @@
 @echo off
 REM bsp colwood.e colwood.bin -l3 -V
+REM bsp colwoodx.e colwoodx.bin -l3 -V
+REM bsp colwoody.e colwoody.bin -l3 -V
 copy /y colwood.bin ..\obj\
+copy /y colwoodx.bin ..\obj\
+copy /y colwoody.bin ..\obj\
 copy /y colwood.png ..\obj\txt16\
+copy /y colwoodx.png ..\obj\txt16\
+copy /y colwoody.png ..\obj\txt16\
---
+++ objsrc/make_rugs.bat
@@ -0,0 +1,7 @@
+@echo off
+copy /y rug01.png ..\obj\txt16\
+copy /y rug01x.png ..\obj\txt16\
+copy /y rug01y.png ..\obj\txt16\
+copy /y rug03.png ..\obj\txt16\
+copy /y rug03x.png ..\obj\txt16\
+copy /y rug03y.png ..\obj\txt16\
