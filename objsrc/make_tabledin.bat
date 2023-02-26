17e04f9251d3be68094ab6c863ed7991c5800224
Andy Durdin <me@andy.durdin.net>
2023-02-26 18:38:20 +0000

    ruined dining hall furniture.

--- objsrc/make_tabledin.bat
+++ objsrc/make_tabledin.bat
@@ -1,6 +1,11 @@
 @echo off
 REM bsp tabledin.e tabledin.bin -l3 -V
+REM bsp tabledinx.e tabledinx.bin -l3 -V
 REM bsp tabledin2.e tabledin2.bin -l3 -V
+REM bsp tabledin2x.e tabledin2x.bin -l3 -V
 copy /y tabledin.bin ..\obj\
+copy /y tabledinx.bin ..\obj\
 copy /y tabledin2.bin ..\obj\
+copy /y tabledin2x.bin ..\obj\
 copy /y tabledin.png ..\obj\txt16\
+copy /y tabledinx.png ..\obj\txt16\
