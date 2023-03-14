421b4a57eecee4bae2148371796f0cbbe1b6baa0
Andy Durdin <me@andy.durdin.net>
2023-03-14 03:23:31 +0000

    more bookpile models.

--- objsrc/make_bookpile.bat
+++ objsrc/make_bookpile.bat
@@ -1,4 +1,10 @@
 @echo off
 REM bsp bookpile.e bookpile.bin -l3 -V
+REM bsp bookpile2.e bookpile2.bin -l3 -V
+REM bsp bookpile3.e bookpile3.bin -l3 -V
+REM bsp bookpile4.e bookpile4.bin -l3 -V
 copy /y bookpile.bin ..\obj\
+copy /y bookpile2.bin ..\obj\
+copy /y bookpile3.bin ..\obj\
+copy /y bookpile4.bin ..\obj\
 copy /y bookpile.png ..\obj\txt16\
