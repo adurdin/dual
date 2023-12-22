a9058cec2d9ce865686e1cd5abd2817dffceb3ac
Andy Durdin <me@andy.durdin.net>
2023-12-22 02:05:58 +0000

    fourth island slice. updated automerge.

--- automerge.bat
+++ automerge.bat
@@ -42,8 +42,10 @@ if not exist %script% goto noscript
 if exist %FMDIR%\_slice1.mis del %FMDIR%\_slice1.mis
 if exist %FMDIR%\_slice2.mis del %FMDIR%\_slice2.mis
 if exist %FMDIR%\_slice3.mis del %FMDIR%\_slice3.mis
+if exist %FMDIR%\_slice4.mis del %FMDIR%\_slice4.mis
 if exist %FMDIR%\_combined1.mis del %FMDIR%\_combined1.mis
 if exist %FMDIR%\_combined2.mis del %FMDIR%\_combined2.mis
+if exist %FMDIR%\_combined3.mis del %FMDIR%\_combined3.mis
 
 pushd %T2DIR%
 dromed.exe -fm=%FM% edit_script_StartUp=%script% mergename=%MISNAME% mergeoptim=1 mergelighting=0
@@ -53,23 +55,28 @@ popd
 if not exist %FMDIR%\_slice1.mis goto badmerge
 if not exist %FMDIR%\_slice2.mis goto badmerge
 if not exist %FMDIR%\_slice3.mis goto badmerge
+if not exist %FMDIR%\_slice4.mis goto badmerge
 
 echo time to merge %FMDIR%\_slice1.mis %FMDIR%\_slice2.mis etc...
 
-misdeed merge _slice1.mis _slice2.mis 0 0 1 -416 -o _combined1.mis
+misdeed merge _slice1.mis _slice2.mis 0 0 1 -512 -o _combined1.mis
 if not exist %FMDIR%\_combined1.mis goto badmerge
-misdeed merge _combined1.mis _slice3.mis 0 0 1 96 -o _combined2.mis
+misdeed merge _combined1.mis _slice3.mis 0 0 1 0 -o _combined2.mis
 if not exist %FMDIR%\_combined2.mis goto badmerge
+misdeed merge _combined2.mis _slice4.mis 0 0 1 512 -o _combined3.mis
+if not exist %FMDIR%\_combined3.mis goto badmerge
 
 REM looks good! lets write over the original mission now, and clean up
-rem copy /y %FMDIR%\_combined2.mis %FMDIR%\%MISNAME%.mis
+rem copy /y %FMDIR%\_combined3.mis %FMDIR%\%MISNAME%.mis
 
 REM -- we could clean up , but lets leave the output files here for debugging.
 rem if exist %FMDIR%\_slice1.mis del %FMDIR%\_slice1.mis
 rem if exist %FMDIR%\_slice2.mis del %FMDIR%\_slice2.mis
 rem if exist %FMDIR%\_slice3.mis del %FMDIR%\_slice3.mis
+rem if exist %FMDIR%\_slice4.mis del %FMDIR%\_slice4.mis
 rem if exist %FMDIR%\_combined1.mis del %FMDIR%\_combined1.mis
 rem if exist %FMDIR%\_combined2.mis del %FMDIR%\_combined2.mis
+rem if exist %FMDIR%\_combined3.mis del %FMDIR%\_combined3.mis
 
 goto end
 
